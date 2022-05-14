c0eef339a5931e585b00125e2e08cf20e7fd69f3
Nikita-Eception <gladkovnikita06@gmail.com>
2022-05-14 13:43:14 +0300

    Implement ProductService and created test for him

---
+++ src/main/java/org/nikita/chiken_bell/core/service/impl/ProductServiceImpl.java
@@ -0,0 +1,56 @@
+package org.nikita.chiken_bell.core.service.impl;
+
+import org.nikita.chiken_bell.core.entity.Product;
+import org.nikita.chiken_bell.core.exception.ProductNotFoundException;
+import org.nikita.chiken_bell.core.service.ProductService;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class ProductServiceImpl implements ProductService {
+
+    private final Set<Product> products = new HashSet<>();
+
+    @Override
+    public Product create(String title, BigDecimal price) {
+        checkProduct(title, price);
+        Product product = new Product(title, price);
+        products.add(product);
+        return product;
+    }
+
+    @Override
+    public Product update(String id, String title, BigDecimal price) {
+        Product product = getById(id).orElseThrow(ProductNotFoundException::new);
+        products.remove(product);
+        product.setTitle(title);
+        product.setPrice(price);
+        products.add(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> getById(String id) {
+        return Optional.of(products.stream().filter(x -> x.getId().equals(id)).findFirst().orElseThrow(ProductNotFoundException::new));
+    }
+
+    @Override
+    public Collection<Product> getAll() {
+        return getProducts();
+    }
+
+    @Override
+    public void deleteById(String id) {
+        products.remove(getById(id).orElseThrow(ProductNotFoundException::new));
+    }
+
+    public Set<Product> getProducts(){
+        return Collections.unmodifiableSet(products);
+    }
+
+    private void checkProduct(String title, BigDecimal price){
+        if (title.isBlank() || price == null){
+            throw new NullPointerException();
+        }
+    }
+}
---
+++ src/test/java/org/nikita/chiken_bell/core/service/ProductServiceTest.java
@@ -0,0 +1,78 @@
+package org.nikita.chiken_bell.core.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.nikita.chiken_bell.core.entity.Product;
+import org.nikita.chiken_bell.core.exception.ProductNotFoundException;
+import org.nikita.chiken_bell.core.service.impl.ProductServiceImpl;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    private final ProductServiceImpl service = new ProductServiceImpl();
+    private Product prod;
+
+    private static final String TITLE = "Apple";
+
+    @BeforeEach
+    void initSet() {
+        prod = service.create("Milk", BigDecimal.TEN);
+    }
+
+    @Test
+    void testCreate() {
+        assertEquals(1, service.getProducts().size());
+        Product product = service.create(TITLE, BigDecimal.TEN);
+
+        assertNotNull(service.getById(product.getId()));
+        assertEquals(2, service.getProducts().size());
+    }
+
+    @Test
+    void testUpdate() {
+        Product product = service.create(TITLE, BigDecimal.ONE);
+        Product update = service.update(product.getId(), "Update", BigDecimal.TEN);
+
+        Product get = service.getById(product.getId()).orElseThrow(ProductNotFoundException::new);
+
+        assertEquals(product.getId(), update.getId());
+
+        assertEquals(update.getTitle(), get.getTitle());
+        assertEquals(update.getPrice(), get.getPrice());
+    }
+
+    @Test
+    void testGetById() {
+            Product product = service.getById(prod.getId()).orElseThrow(ProductNotFoundException::new);
+
+            assertEquals(prod.getTitle(), product.getTitle());
+            assertEquals(prod.getPrice(), product.getPrice());
+    }
+
+    @Test
+    void testGetAll() {
+        assertEquals(1, service.getAll().size());
+
+        service.create(TITLE, BigDecimal.TEN);
+
+        assertEquals(2, service.getAll().size());
+    }
+
+    @Test
+    void deleteById() {
+        assertEquals(1, service.getAll().size());
+
+        service.deleteById(prod.getId());
+
+        assertEquals(0, service.getAll().size());
+
+    }
+
+    @Test
+    void testInitProductServiceWithNull(){
+        assertThrows(NullPointerException.class, () -> service.create(null, BigDecimal.TEN));
+    }
+}
